202ad5ecfc4e0b37d0c61865d586efbee2d3de8f
Anton Kovalyov <anton@kovalyov.net>
2012-12-13 17:13:53 -0800

    Added MPL blocks to all Cleopatra files

--- browser/devtools/profiler/cleopatra/js/parserWorker.js
+++ browser/devtools/profiler/cleopatra/js/parserWorker.js
@@ -1,5 +1,9 @@
 /* -*- Mode: js2; indent-tabs-mode: nil; js2-basic-offset: 2; -*- */
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 importScripts("ProgressReporter.js");
 
 var gProfiles = [];
